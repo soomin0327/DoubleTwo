f088ec7277875919ac612458fc8cca7460be4afe
SuminGo <jennifer045020@gmail.com>
2018-04-15 23:49:37 +0900

    Create Activities

---
+++ DoubleTwo/app/src/main/java/com/example/dsm2016/doubletwo/IntroActivity.java
@@ -0,0 +1,13 @@
+package com.example.dsm2016.doubletwo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
