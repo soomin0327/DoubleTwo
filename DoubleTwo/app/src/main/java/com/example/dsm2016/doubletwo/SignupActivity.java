1e84535cacd03b7b2bb15a4b71f478b319b531d9
SuminGo <jennifer045020@gmail.com>
2018-04-16 22:50:21 +0900

    Complete Splash and Complete Signup xml

--- DoubleTwo/app/src/main/java/com/example/dsm2016/doubletwo/SignupActivity.java
+++ DoubleTwo/app/src/main/java/com/example/dsm2016/doubletwo/SignupActivity.java
@@ -1,13 +1,19 @@
 package com.example.dsm2016.doubletwo;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
 public class SignupActivity extends AppCompatActivity {
+    List<Drawable> temp;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
     }
 }
--- DoubleTwo/app/src/main/java/com/example/dsm2016/doubletwo/SplashActivity.java
+++ DoubleTwo/app/src/main/java/com/example/dsm2016/doubletwo/SplashActivity.java
@@ -1,6 +1,25 @@
 package com.example.dsm2016.doubletwo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    static final int DELAY_SECOND = 3000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toIntro = new Intent(SplashActivity.this, SignupActivity.class);
+                startActivity(toIntro);
+            }
+        }, DELAY_SECOND);
+    }
 }
