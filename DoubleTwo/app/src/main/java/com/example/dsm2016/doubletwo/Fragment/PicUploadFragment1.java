71e4e1a52ab408c665363e4443f549cd8415abde
SuminGo <jennifer045020@gmail.com>
2018-04-20 23:51:45 +0900

    no err

--- DoubleTwo/app/src/main/java/com/example/dsm2016/doubletwo/Fragment/PicUploadFragment1.java
+++ DoubleTwo/app/src/main/java/com/example/dsm2016/doubletwo/Fragment/PicUploadFragment1.java
@@ -1,9 +1,9 @@
 package com.example.dsm2016.doubletwo.Fragment;
 
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +11,8 @@ import android.view.ViewGroup;
 import com.example.dsm2016.doubletwo.R;
 
 public class PicUploadFragment1 extends Fragment {
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_pic_upload1, container, false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pic_upload1, container, false);
     }
 }
